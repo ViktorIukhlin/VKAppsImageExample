8e523ccd5b7a8900ab1adb77ebebad92fed279b7
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-09-14 17:18:20 +0300

    Updates

---
+++ precache-manifest.c23bb319d9124d556fda669c96b4beaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b8d30d4e62debbe17f0759cdb14f18",
+    "url": "/VKAppsImageExample/index.html"
+  },
+  {
+    "revision": "c69a84717bc643c6337b",
+    "url": "/VKAppsImageExample/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "c69a84717bc643c6337b",
+    "url": "/VKAppsImageExample/static/js/2.526297dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VKAppsImageExample/static/js/2.526297dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d91f33a8328826a1468c",
+    "url": "/VKAppsImageExample/static/js/main.373d34f7.chunk.js"
+  },
+  {
+    "revision": "8a0674c463a655dd3103",
+    "url": "/VKAppsImageExample/static/js/runtime-main.a1f6ecf9.js"
+  }
+]);
